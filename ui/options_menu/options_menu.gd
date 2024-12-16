eabc3135e118561c01a23c27198519363aa3fc49
Oliver Voogd <51314988+OliverVoogd@users.noreply.github.com>
2024-12-16 17:03:26 +1100

    reduced unneccesary scripts for showing/hiding colour picker and changing colours

--- ui/options_menu/options_menu.gd
+++ ui/options_menu/options_menu.gd
@@ -6,32 +6,36 @@ signal algorithm_changed
 @onready var algorithm_option_button := $VBoxContainer/AlgorithmOption
 
 @onready var picker := $ColorPicker
+var color_rects: Array[ColorRect]
 
 func _ready() -> void:
 	algorithm_option_button.clear()
 	for algo in algorithms:
 		algorithm_option_button.add_item(algo.algorithm_name)
-		
+	
 	# add the colour rect signals
-	getNeutralColorRect().colour_rect_clicked.connect(on_colour_rect_clicked)
-	getCurrentColorRect().colour_rect_clicked.connect(on_colour_rect_clicked)
-	getBoundaryColorRect().colour_rect_clicked.connect(on_colour_rect_clicked)
-	getExploredColorRect().colour_rect_clicked.connect(on_colour_rect_clicked)
-
-func on_colour_rect_clicked(rect: ColorRect):
-	picker.make_visible(rect)
+	color_rects = [
+		getNeutralColorRect(), getCurrentColorRect(), getBoundaryColorRect(), getExploredColorRect()
+	]
+	for rect in color_rects:
+		rect.gui_input.connect(on_color_rect_clicked.bind(rect))
 
 func _on_algorithm_option_item_selected(index: int) -> void:
 	#algorithm_changed.emit(algorithms[index])
 	pass
 
-func getNeutralColorRect():
-	return $VBoxContainer/GridContainer/crRectNeutral
-func getCurrentColorRect():
-	return $VBoxContainer/GridContainer/crRectCurrent
-func getBoundaryColorRect():
-	return $VBoxContainer/GridContainer/crRectBoundary
-func getExploredColorRect():
-	return $VBoxContainer/GridContainer/crRectExplored
+#region Color Rects
+func on_color_rect_clicked(event: InputEvent, rect: ColorRect):
+	if (event.is_action_pressed("left_mouse")):
+		picker.make_visible(rect)
 
+func getNeutralColorRect() -> ColorRect:
+	return $VBoxContainer/GridContainer/crRectNeutral
+func getCurrentColorRect() -> ColorRect:
+	return $VBoxContainer/GridContainer/crRectCurrent
+func getBoundaryColorRect() -> ColorRect:
+	return $VBoxContainer/GridContainer/crRectBoundary
+func getExploredColorRect() -> ColorRect:
+	return $VBoxContainer/GridContainer/crRectExplored
+#endregion
 
